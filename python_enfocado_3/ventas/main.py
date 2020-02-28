d6f8fc84bf2aab9d3aac8cdc32cf3cabf9f7a5d2
andru <andru2400@hotmail.com>
2020-02-28 12:07:51 -0500

    Crud Funcional con archivo csv

--- python_enfocado_3/ventas/main.py
+++ python_enfocado_3/ventas/main.py
@@ -1,21 +1,28 @@
 import sys
+import csv
+import os
 
 
-clients = [
-    {
-        'name': 'Pablo',
-        'company': 'Google',
-        'email': 'pablo@google.com',
-        'position': 'Software Engineer',
-    },
-    {
-        'name': 'Ricardo',
-        'company': 'Facebook',
-        'email': 'ricardo@facebook.com',
-        'position': 'Data Engineer',
-    },
-]
+CLIENT_TABLE = 'clients.csv'
+CLIENT_SHEMA = ['name','company','email', 'position']
+clients = []
 
+
+def _initialize_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader =  csv.DictReader(f, fieldnames=CLIENT_SHEMA)
+        for row in reader:
+            clients.append(row)
+
+
+def _save_clients_to_storage():
+    tmp_table_name = '{}.tmp'.format(CLIENT_TABLE)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SHEMA)
+        writer.writerows(clients)
+        os.remove(CLIENT_TABLE)
+        f.close()
+        os.rename(tmp_table_name, CLIENT_TABLE)
 
 def create_client(client):
     global clients
@@ -94,6 +101,8 @@ def _print_welcome():
 
 
 if __name__ == '__main__':
+    _initialize_clients_from_storage()
+    
     _print_welcome()
 
     command = input()
@@ -101,20 +110,18 @@ if __name__ == '__main__':
 
     if command == 'C':
         client = _get_client_from_user()
-        create_client(client)
-        list_clients()
+        create_client(client)        
     elif command == 'L':
         list_clients()
     elif command == 'U':
         client_id = int(_get_client_field('id'))
         updated_client = _get_client_from_user()
-
         update_client(client_id, updated_client)
-        list_clients()
+        
     elif command == 'D':
         client_id = int(_get_client_field('id'))
         delete_client(client_id)
-        list_clients()
+        
     elif command == 'S':
         client_name = _get_client_field('name')
         found = search_client(client_name)
@@ -125,3 +132,5 @@ if __name__ == '__main__':
             print('The client: {} is not in our client\'s list'.format(client_name))
     else:
         print('Invalid command')
+
+    _save_clients_to_storage()
